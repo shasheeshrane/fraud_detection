3ef698021e96869c214fb3e5e4e78c79744e2b43
shasheeshrane <50438284+shasheeshrane@users.noreply.github.com>
2022-01-26 02:12:13 +0530

    Update fraud_detection.py

--- fraud_detection.py
+++ fraud_detection.py
@@ -1,1 +1,14 @@
-print("Adding all the code for fraud detection")
+#### This code is copied taken from https://www.tutorialspoint.com/fraud-detection-in-python 
+#### This is meant strictly for Study Purposes
+
+import pandas as pd
+#Load the creditcard.csv using pandas
+datainput = pd.read_csv("s3://myawscloud.uk/creditcard.csv")
+false = datainput[datainput['Class'] == 1]
+true = datainput[datainput['Class'] == 0]
+n = len(false)/float(len(true))
+print(n)
+print('False Detection Cases: {}'.format(len(datainput[datainput['Class'] == 1])))
+print('True Detection Cases: {}'.format(len(datainput[datainput['Class'] == 0])),"\n")
+
+print("This code is copied taken from https://www.tutorialspoint.com/fraud-detection-in-python ")
